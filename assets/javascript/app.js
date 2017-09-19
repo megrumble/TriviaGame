49a8bd86f05310cc0c451141f7e912f0b1e6ac39
Margaret Rumble <megrumble@gmail.com>
2017-09-19 15:06:39 -0400

    initial

---
+++ assets/javascript/app.js
@@ -0,0 +1,82 @@
+// var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+
+var trivia1 = {
+    "question": "What gives green pasta its color?",
+    "answer": "Spinach",
+};
+
+var trivia2 = {
+    "question": "What is the salted roe of a sturgeon called?",
+    "answer": "Caviar",
+};
+
+
+var trivia3 = {
+    "question": "Which vegetable is a green variety of banana, used as a staple food in the tropics?",
+    "answer": "Plantain",
+};
+
+var trivia4 = {
+    "question": "What type of flowers produce vanilla pods?",
+    "answer": "Orchids",
+};
+
+var trivia5 = {
+    "question": "For which edible fungus is Perigord, France famous?",
+    "answer": "Truffles",
+};
+
+var trivia6 = {
+    "question": "What are dried plums called?",
+    "answer": "Prunes",
+};
+
+var triviaArr = [trivia1, trivia2, trivia3, trivia4, trivia5, trivia6, ];
+var time;
+var correct;
+var wrong;
+var oot;
+console.log(trivia1);
+// window.onload = function () {
+//     secondsLeft = 30;
+//     $("#timer").html(secondsLeft);
+//     return;
+// };
+//start button
+$(".start").on("click", function () {
+    correct = 0;
+    wrong = 0;
+    oot = 0;
+    secondsLeft = 30;
+    clock();
+    $("#timer").html(secondsLeft);
+    $(".game").html(trivia1.queston);
+    console.log(secondsLeft);
+});
+//reset between questions
+var timeLeft = 3;
+var reset = setInterval(function () {
+    timeLeft--;
+    if (timeLeft <= 0) {
+        clearInterval(reset);
+    }
+}, 1000);
+
+
+//countdown timer
+var secondsLeft = 30;
+
+
+var timerId = setInterval(countdown, 1000);
+
+var clock=function countdown() {
+    if (secondsLeft < 0) {
+        clearInterval(timerId);
+        $("#timer").html("Time's up!");
+    } else {
+        $("#timer").html(secondsLeft);
+        secondsLeft--;
+    }
+
+}
